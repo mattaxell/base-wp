1052417f94933e7ba40c1dd19441f9831d6b3ac5
Matt Axell <hello@mattaxell.com>
2014-09-26 15:53:04 +0100

    Add config options section

--- lib/_config.php
+++ lib/_config.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+* Site options
+* ============
+* Define which Wordpress functions to include
+* as well as some custom functionality
+*/
+$theme_config = array(
+    'enable_post_thumbnails'     => true,
+    'enable_custom_menu'         => true,
+    'remove_wp_head_info'        => true,
+    'remove_unwanted_menu_items' => true,
+    'enable_admin_bar'           => false,
+    'enable_custom_login'        => true
+);
+
 // Define constants
 define('THEME', '/wp-content/themes/tannwestlake');
 define('IMG', THEME . '/public/img');
