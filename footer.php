42a25d07683969858945f76973ba820d59335461
Matt Axell <hello@mattaxell.com>
2014-09-26 13:29:01 +0100

    Add jQuery to footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="<?php print JS; ?>/main.min.js"></script>
+
+        <?php wp_footer(); ?>
+
+    </body>
+
+</html>
