be87d3ea3568269c653ab52cfface881b7947650
Matt Axell <hello@mattaxell.com>
2014-09-26 16:00:16 +0100

    Add conditional statements for custom functionality based on config file

--- lib/wp-setup.php
+++ lib/wp-setup.php
@@ -3,38 +3,58 @@
 // Post thumbnail support
 // ============================================== //
 
-add_theme_support('post-thumbnails');
-/* add_image_size('', XXX, XXX, true); */
+if ( $theme_config['enable_post_thumbnails'] === true ) {
+
+    add_theme_support('post-thumbnails');
+    /* add_image_size('', XXX, XXX, true); */
+
+}
 
 // Custom menu support
 // ============================================== //
 
-function register_my_menu() {
-    register_nav_menu( 'main-nav', 'Main Navigation' );
-}
+if ( $theme_config['enable_custom_menu'] === true ) {
 
-add_action( 'init', 'register_my_menu' );
+    function register_my_menu() {
+        register_nav_menu( 'main-nav', 'Main Navigation' );
+    }
+
+    add_action( 'init', 'register_my_menu' );
+
+}
 
 // Remove WP Head Stuff
 // ============================================== //
 
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'index_rel_link');
+if ( $theme_config['remove_wp_head_info'] === true ) {
+
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'index_rel_link');
+
+}
 
 // Disable admin bar
 // ============================================== //
 
-add_filter('show_admin_bar', '__return_false');
+if ( $theme_config['enable_admin_bar'] === false ) {
 
-// Remove unwanter wp admin menu items
-// ============================================== //
+    add_filter('show_admin_bar', '__return_false');
 
-function remove_menu_pages() {
-    remove_menu_page('edit-comments.php');
-    remove_menu_page('upload.php');
 }
 
-add_action( 'admin_menu', 'remove_menu_pages' );
+// Remove unwanted wp admin menu items
+// ============================================== //
+
+if ( $theme_config['remove_unwanted_menu_items'] === true ) {
+
+    function remove_menu_pages() {
+        remove_menu_page('edit-comments.php');
+        remove_menu_page('upload.php');
+    }
+
+    add_action( 'admin_menu', 'remove_menu_pages' );
+
+}
