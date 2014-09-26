8b688262b1f4d927dc51f1dfec65663c64f7236f
Matt Axell <hello@mattaxell.com>
2014-09-26 16:33:01 +0100

    Fix typo in inclusion on wp-setup in functions file

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once('lib/_config.php');
-include_once('lib/wp-setup');
+include_once('lib/wp-setup.php');
 include_once('lib/helpers.php');
 include_once ('lib/custom-excerpts.php');
 include_once('lib/custom-login.php');
