e0f87d6e6ea54b6d01bde0537ffd2fb7fddd3e72
Matt Axell <hello@mattaxell.com>
2014-09-26 13:25:57 +0100

    Add basic information to header

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> <?php wp_title('|', true, right); ?><?php bloginfo('name'); ?> &ndash; <?php bloginfo('description'); ?></title>
+        <!-- Icons -->
+        <link rel="icon" href="<?php print THEME; ?>/favicon.ico" />
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="<?php print THEME; ?>/public/styles/main.css" />
+        <!-- Fonts -->
+        <!-- INSERT FONT CODE HERE -->
+
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <![endif]-->
+
+        <?php wp_head(); ?>
+
+    </head>
+
+    <body>
