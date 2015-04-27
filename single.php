02d11c606effc4e3c155ae3ef35a29187b9fd6fc
mattaxell <hello@mattaxell.com>
2015-04-27 12:26:24 +0100

    Add blank single template with documentation

---
+++ single.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+    * Single post template
+    * ====================
+    * Template used to display a single blog post and it's content
+    */
+   get_header();
+?>
+
+<?php get_footer(); ?>
