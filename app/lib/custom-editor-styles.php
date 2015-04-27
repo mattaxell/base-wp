4b37190ac0507f74897919cea67fee252ab96428
mattaxell <hello@mattaxell.com>
2015-04-27 12:04:12 +0100

    Add function to allow custom editor styles

---
+++ app/lib/custom-editor-styles.php
@@ -0,0 +1,10 @@
+<?php
+function vl_add_editor_styles() {
+    // Use font from Google Fonts
+    // $font_url = str_replace( ',', '%2C', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,400italic,600italic' );
+    // add_editor_style( $font_url );
+
+    add_editor_style( 'public/styles/editor.css' );
+}
+
+add_action( 'admin_init', 'vl_add_editor_styles' );
