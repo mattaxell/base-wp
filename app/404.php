feb20621efb639e44d019df2e43e93a26155073b
mattaxell <hello@mattaxell.com>
2015-04-27 12:19:41 +0100

    Add 404 template with suggested content

---
+++ app/404.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+    * 404 page template
+    * =================
+    * Template used when content cannot be found
+    */
+   get_header();
+?>
+
+<!--
+<article>
+    <p>The content you are looking for doesn't exist, or may have been moved. Return to the <a href="/">home page</a> or try one of the following:-</p>
+    <ul>
+        <li><a href="/">Page</a></li>
+    </ul>
+    <h4>Alternatively try searching for what you are looking for...</h4>
+    <?php // get_search_form(); ?>
+</article>
+-->
+
+<?php get_footer(); ?>
