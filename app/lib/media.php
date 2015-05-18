7ea14124ee6b2d5aac100c2e75baea153782e393
mattaxell <hello@mattaxell.com>
2015-05-18 13:16:54 +0100

    Update default size of large media inserts

--- app/lib/media.php
+++ app/lib/media.php
@@ -6,7 +6,11 @@
 function defaultMediaSetup() {
     update_option('image_default_align', 'none' ); // No alignment
     update_option('image_default_link_type', 'none' ); // No link
-    update_option('image_default_size', 'full' ); // Full size
+    update_option('image_default_size', 'large' ); // Full size
+
+    // Update default size of 'large' to 1500px wide, unlimited height
+    update_option('large_size_w', 1500);
+    update_option('large_size_h', 9999);
 
 }
 add_action('after_setup_theme', 'defaultMediaSetup');
