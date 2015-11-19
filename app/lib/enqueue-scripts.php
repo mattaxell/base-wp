3b2632a1cbeeaf92093b9f1fbf0cba632ab28b9d
mattaxell <hello@mattaxell.com>
2015-11-19 17:01:18 +0000

    Enqueue scripts properly – d'oh

---
+++ app/lib/enqueue-scripts.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Enqueue all necessary scripts to site
+ * and define dependencies
+ */
+function queue_scripts() {
+
+    // De-register default jQuery inclusion
+    wp_deregister_script('jquery');
+    // Manaully queue jQuery
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', '', null, true);
+    // Load Google maps API on contact page only
+    // if(is_page('contact')) { wp_enqueue_script('google_maps', 'https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js', array('jquery'), null, true); }
+    // Queue main scripts file
+    wp_enqueue_script('main_js', get_template_directory_uri() . '/public/js/main.min.js', array('jquery'), null, true);
+}
+
+if (!is_admin()) {
+    add_action('wp_enqueue_scripts', 'queue_scripts', 11);
+}
--- footer.php
+++ footer.php
@@ -1,11 +1,3 @@
-        <!-- JS -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-        <script src="<?php print JS; ?>/main.min.js"></script>
-        <!-- /JS -->
-
-        <!-- Google Analytics -->
-        <!-- INSERT GA CODE -->
-
         <?php wp_footer(); ?>
 
     </body>
