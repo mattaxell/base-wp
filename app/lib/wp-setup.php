426a7fb4b07fc5c1b52c539a11e08cd37d28b628
mattaxell <hello@mattaxell.com>
2015-11-19 17:17:29 +0000

    Refine basic theme setup

--- app/lib/wp-setup.php
+++ app/lib/wp-setup.php
@@ -1,60 +1,31 @@
 <?php
+
 /**
- * Add title tag support
+ * Enable basic, but additional
+ * WordPress functionality
  */
-if ( $theme_config['enable_title_tag'] === true ) {
+function wp_theme_setup() {
+    // Title tag support
     add_theme_support('title-tag');
-}
-
-/**
- * Add post thumbnail support
- * ==========================
- * Register any additional image sizes with add_image_size()
- */
-if ( $theme_config['enable_post_thumbnails'] === true ) {
+    // Thumbnail support
     add_theme_support('post-thumbnails');
-    // add_image_size('square', 340, 340, true);
-}
-
-/**
- * Add custom navigation support
- * =============================
- * Add individual menus with register_nav_menu()
- */
-if ( $theme_config['enable_custom_menu'] === true ) {
-    function register_my_menu() {
-        register_nav_menu( 'main-nav', 'Main Navigation' );
-        register_nav_menu( 'footer-nav', 'Footer Navigation' );
-    }
-    add_action( 'init', 'register_my_menu' );
-}
-
-/**
- * Remove WP Head info
- */
-if ( $theme_config['remove_wp_head_info'] === true ) {
+    // Register Custom Menus
+    register_nav_menu( 'main-nav', 'Main Navigation' );
+    register_nav_menu( 'footer-nav', 'Footer Navigation' );
+    // Remove WP Head Info
     remove_action('wp_head', 'wp_generator');
     remove_action('wp_head', 'wlwmanifest_link');
     remove_action('wp_head', 'feed_links_extra', 3);
     remove_action('wp_head', 'rsd_link');
     remove_action('wp_head', 'index_rel_link');
 }
+add_action('after_setup_theme', 'wp_theme_setup');
 
 /**
- * Disable admin bar
+ * Prevent user from editing the theme
+ * within the WP appearance menu
  */
-if ( $theme_config['enable_admin_bar'] === false ) {
-    add_filter('show_admin_bar', '__return_false');
+function remove_editor_menu() {
+    remove_action('admin_menu', '_add_themes_utility_last', 101);
 }
-
-/**
- * Disable theme editor
- * ====================
- * Prevent user from editing the theme in the admin area
- */
-if ( $theme_config['enable_theme_editor'] === false ) {
-    function remove_editor_menu() {
-        remove_action('admin_menu', '_add_themes_utility_last', 101);
-    }
-    add_action('_admin_menu', 'remove_editor_menu', 1);
-}
+add_action('_admin_menu', 'remove_editor_menu', 1);
--- functions.php
+++ functions.php
@@ -1,18 +1,22 @@
 <?php
 
-// Config
-include_once('app/_config.php');
+// Define constants
+define('THEME', get_stylesheet_directory_uri());
+define('THEME_PATH', realpath(__DIR__));
+define('IMG', THEME . '/public/img');
+define('STYLES', THEME . 'public/styles');
+define('JS', THEME . '/public/js');
 
-// Library
-include_once('app/lib/wp-setup.php');
-include_once('app/lib/helpers.php');
-include_once('app/lib/clean-nav.php');
+// Include library items
 include_once('app/lib/admin-menu.php');
+include_once('app/lib/clean-nav.php');
 include_once('app/lib/custom-editor-styles.php');
-include_once('app/lib/custom-excerpts.php');
 include_once('app/lib/custom-login.php');
+include_once('app/lib/custom-excerpts.php');
 include_once('app/lib/editor.php');
+include_once('app/lib/helpers.php');
 include_once('app/lib/media.php');
+include_once('app/lib/wp-setup.php');
 
-// ACF
+// Configure use of ACF Pro
 include_once('app/acf/acf-config.php');
