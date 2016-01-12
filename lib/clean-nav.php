5be3724ae7287f10e8a067216709aae08f43492c
mattaxell <hello@mattaxell.com>
2016-01-12 14:10:47 +0000

    Add further navigation helper functions for clean output of wp_nav_menus

--- lib/clean-nav.php
+++ lib/clean-nav.php
@@ -46,3 +46,43 @@ function strip_empty_classes($menu){
 }
 
 add_filter ('wp_nav_menu','strip_empty_classes');
+
+/**
+ * Update options from wp_nav_menu to
+ * output cleanest markup possible
+ * @param  string $location Theme location of menu required
+ * @return string           Markup of menu
+ */
+function clean_nav_menu($location) {
+    $args = array(
+        'theme_location'  => $location,
+        'menu'            => '',
+        'container'       => '',
+        'container_class' => '',
+        'container_id'    => '',
+        'menu_class'      => '',
+        'menu_id'         => '',
+        'items_wrap'      => '<ul>%3$s</ul>'
+    );
+
+    echo wp_nav_menu($args);
+}
+
+/**
+ * Return raw list of links from a
+ * wp_nav_menu by passing theme location
+ * @param  string $location Name of the menu
+ * @return string           List of anchor tags
+ */
+function menu_item_links($location) {
+    if(($locations = get_nav_menu_locations()) && isset($locations[$location])) {
+        $menu = wp_get_nav_menu_object($locations[$location]);
+        $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+        foreach((array) $menu_items as $key => $menu_item) {
+            $url = $menu_item->url;
+            $title = $menu_item->title;
+            echo '<a href="'. $url .'">'. $title .'</a>' ."\n";
+        }
+    }
+}
