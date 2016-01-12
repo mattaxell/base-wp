59d3d008e6bd9abd29d8df1c21b71878b009c2c7
mattaxell <hello@mattaxell.com>
2016-01-12 14:12:44 +0000

    Remove shake animation after failed login attempt

--- lib/custom-login.php
+++ lib/custom-login.php
@@ -5,12 +5,21 @@
  * @return str html link to stylesheet
  */
 function custom_login() {
-    $files = '<link rel="stylesheet" href="'.get_bloginfo('template_directory').'/public/styles/login.css" />';
+    $files = '<link rel="stylesheet" href="'.get_bloginfo('template_directory').'/assets/css/login.min.css" />';
     // Echo link to custom stylesheet
     echo $files;
 }
 
 add_action('login_head', 'custom_login');
+
+/**
+ * Remove shake effect on unsuccesful login attempts
+ */
+function no_shakes_please() {
+    remove_action('login_head', 'wp_shake_js', 12);
+}
+
+add_action('login_head', 'no_shakes_please');
 
 /**
  * Remove link to wordpress.org from login logo
