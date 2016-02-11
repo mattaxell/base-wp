6c7945dcbb061a53153d0384f7d70de369c6edf0
mattaxell <hello@mattaxell.com>
2016-02-11 16:20:28 +0000

    Add function to check for user role(s)

--- lib/helpers.php
+++ lib/helpers.php
@@ -23,6 +23,24 @@ function get_partial($path) {
 }
 
 /**
+ * Conditional check for the current user role
+ * @param  array $roles List of roles to check
+ * @return bool         Does query match current user role
+ */
+function current_user_role_is($roles) {
+    // Current user object
+    $user = wp_get_current_user();
+    // Array of roles to check for
+    $allowed_roles = array($roles);
+
+    // If roles is matched in array return true, otherwise return false
+    if(array_intersect($allowed_roles, $user->roles)) {
+        return true;
+    }
+    return false;
+}
+
+/**
  * List the terms of a post from a specific taxonomy
  * @param  integer $id  The ID of the post
  * @param  string  $tax Name of the taxonomy to grab the terms of
